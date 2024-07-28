049d195c58b41e412d9d6b69d64b039c2b1ec03f
DarmilaT <darmila02.t@gmail.com>
2024-07-28 14:53:16 +0530

    Add: Navbar links with dropdown

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,53 @@
 import Logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import DarkMode from "./DarkMode";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Electronics",
+    link: "/#",
+  },
+];
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
 
 const Navbar = () => {
   return (
@@ -26,7 +72,7 @@ const Navbar = () => {
                 placeholder="search"
                 className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300
                 rounded-full border border-gray-300 px-2 py-1 focus:outline-none
-                focus:border-1 focus:border-primary"
+                focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800"
               />
               <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
             </div>
@@ -50,7 +96,46 @@ const Navbar = () => {
         </div>
       </div>
       {/* lower Navbar */}
-      <div></div>
+      <div className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-5">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a
+                href={data.link}
+                className="inline-block px-4 hover:text-primary duration-200"
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          {/* Simple Dropdown and lists */}
+          <li className="group relative cursor-pointer">
+            <a
+              href="#"
+              className="flex items-center gap-[2px] py-2 hover:text-primary"
+            >
+              Trending Products
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
+            </a>
+            <div className="absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black shadow-md">
+              <ul>
+                {DropdownLinks.map((data) => (
+                  <li key={data.id}>
+                    <a
+                      href={data.link}
+                      className="inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
